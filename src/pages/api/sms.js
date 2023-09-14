1890f8d800f42c4c2ab8809699b5d83b87fcd0ef
ronald-luo <r.luo.180@gmail.com>
2023-09-14 18:19:46 -0400

    link gpt

--- src/pages/api/sms.js
+++ src/pages/api/sms.js
@@ -1,25 +1,24 @@
 // import OpenAI from 'openai';
 const { MessagingResponse } = require('twilio').twiml;
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-// async function chatGPTResponse() {
-  
-//   let context = ''
+async function chatGPTResponse() {
+  let context = ''
 
-//   const completion = await openai.chat.completions.create({
-//     messages: [
-//       { role: 'system', content: `${context}` }, 
-//       { role: 'user', content: 'Say this is a test' },
-//       { role: 'assistant', content: 'im sorry i cannot do that' }
-//     ],
-//     model: 'gpt-3.5-turbo',
-//   });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: `${context}` }, 
+      { role: 'user', content: 'Say this is a test' },
+      // { role: 'assistant', content: 'im sorry i cannot do that' }
+    ],
+    model: 'gpt-3.5-turbo',
+  });
 
-//   return (completion.choices[0].message.content);
-// };
+  return (completion.choices[0].message.content);
+};
 
 const sms = async (req, res) => {
   // const twiml = new MessagingResponse();
@@ -29,7 +28,7 @@ const sms = async (req, res) => {
   console.log('hello there')
 
   const twiml = new MessagingResponse();
-  twiml.message('testing twilio');
+  twiml.message(chatGPTResponse());
 
   res.setHeader("Content-Type", "text/xml");
   res.status(200).send(twiml.toString());
