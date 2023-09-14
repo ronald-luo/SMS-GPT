953b0bd4b893467c600ccd08bdd151e6f6f48b58
ronald-luo <r.luo.180@gmail.com>
2023-09-14 18:16:01 -0400

    change res headers

--- src/pages/api/sms.js
+++ src/pages/api/sms.js
@@ -1,50 +1,38 @@
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 const { MessagingResponse } = require('twilio').twiml;
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-async function chatGPTResponse() {
+// async function chatGPTResponse() {
   
-  let context = ''
+//   let context = ''
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: 'system', content: `${context}` }, 
-      { role: 'user', content: 'Say this is a test' },
-      { role: 'assistant', content: 'im sorry i cannot do that' }
-    ],
-    model: 'gpt-3.5-turbo',
-  });
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       { role: 'system', content: `${context}` }, 
+//       { role: 'user', content: 'Say this is a test' },
+//       { role: 'assistant', content: 'im sorry i cannot do that' }
+//     ],
+//     model: 'gpt-3.5-turbo',
+//   });
 
-  return (completion.choices[0].message.content);
-};
+//   return (completion.choices[0].message.content);
+// };
 
 const sms = async (req, res) => {
   // const twiml = new MessagingResponse();
   // twiml.message('testing twilio');
   // res.type('text/xml').send(twiml.toString());
 
+  console.log('hello there')
+
   const twiml = new MessagingResponse();
   twiml.message('testing twilio');
 
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(twiml.toString());
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(twiml.toString());
 }
 
 export default sms;
-
-// Your AccountSID and Auth Token from console.twilio.com
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//     body: 'Hello from twilio-node',
-//     to: '+12345678901', // Text your number
-//     from: '+12345678901', // From a valid Twilio number
-//   })
-//   .then((message) => console.log(message.sid));
