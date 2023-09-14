f946e7f15e71dcf7a4d01ba9fa41c2190bc3c67b
ronald-luo <r.luo.180@gmail.com>
2023-09-14 17:53:19 -0400

    fix import

--- src/pages/api/sms.js
+++ src/pages/api/sms.js
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { MessagingResponse } from "twilio/lib/twiml";
+const { MessagingResponse } = require('twilio').twiml;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
